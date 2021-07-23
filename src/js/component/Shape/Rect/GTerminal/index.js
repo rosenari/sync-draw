483f650dcb6c3b4bf2c2c78f761fb6492cee7b16
rosenari <rosenari88@gmail.com>
2021-07-23 09:18:18 +0900

    모양 컴포넌트 추가

---
+++ src/js/component/Shape/Rect/GTerminal/index.js
@@ -0,0 +1,13 @@
+import Rect from '../index';
+
+export default class GTerminal extends Rect{
+
+    constructor({ parentId, id, x = 0, y = 0, width = 0, height = 0, classList, handlers }) {
+        super({ parentId, id, x, y, width, height, classList, handlers });
+    }
+
+    set height(value) {
+        super.height = value;
+        this.elem.setAttribute('ry', (value * 0.5).toFixed(2));
+    }
+}
---
+++ src/js/component/Shape/Rect/index.js
@@ -0,0 +1,44 @@
+import Shape from '../index';
+
+export default class Rect extends Shape {
+
+    constructor({ parentId, id, x = 0, y = 0, width = 0, height = 0, classList, handlers }) {
+        super({ parentId, id, tagName: 'rect', classList, handlers });
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    get x() {
+        return +this.elem.getAttribute('x');
+    }
+
+    set x(value) {
+        this.elem.setAttribute('x', value);
+    }
+
+    get y() {
+        return +this.elem.getAttribute('y');
+    }
+
+    set y(value) {
+        this.elem.setAttribute('y', value);
+    }
+
+    get width() {
+        return +this.elem.getAttribute('width');
+    }
+
+    set width(value) {
+        this.elem.setAttribute('width', value);
+    }
+
+    get height() {
+        return +this.elem.getAttribute('height');
+    }
+
+    set height(value) {
+        this.elem.setAttribute('height', value);
+    }
+}
