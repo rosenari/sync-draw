4a1c19668c5791e741db526661fd19a9e43271dd
chris-92 <chris.92@kakaocorp.com>
2021-07-27 15:45:44 +0900

    흐름선 생성 및 직선 생성 기능추가

---
+++ src/js/component/Line/GLine/index.js
@@ -0,0 +1,57 @@
+import Line from '../index';
+import GraphicElement from "../../GraphicElement";
+
+class GLine extends Line {
+    _marker = null;
+    _arrow = null;
+
+    constructor({ parentId, id, startX, startY, classList, handlers }){
+        super({
+            parentId,
+            id,
+            startX,
+            startY,
+            classList,
+            handlers
+        });
+
+        this.marker = new GraphicElement({
+            parentId: this.id,
+            id: id + '_marker',
+            tagName: 'marker'
+        });
+        this.marker.elem.setAttribute('markerWidth','5');
+        this.marker.elem.setAttribute('markerHeight','5');
+        this.marker.elem.setAttribute('refX','0');
+        this.marker.elem.setAttribute('refY','1.5');
+        this.marker.elem.setAttribute('orient','auto');
+
+        this.arrow = new GraphicElement({
+            parentId: this.marker.id,
+            id: id + '_arrow',
+            tagName: 'polygon'
+        });
+
+        this.arrow.elem.setAttribute('points','0 0, 5 1.5, 0 3');
+        this.arrow.elem.setAttribute('fill','#555');
+        this.line.elem.setAttribute('marker-end',`url(#${this.marker.id})`);
+    }
+
+    get marker() {
+        return this._marker;
+    }
+
+    set marker(value) {
+        this._marker = value;
+    }
+
+    get arrow() {
+        return this._arrow;
+    }
+
+    set arrow(value) {
+        this._arrow = value;
+    }
+}
+
+export default GLine;
