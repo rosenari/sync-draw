8d5f1ea167f4fd0dca44da954d3af6f429ea5b03
chris-92 <chris.92@kakaocorp.com>
2021-08-02 12:37:18 +0900

    흐름선, 모양 연동기능추가 (+이슈해결: disk fill,선택시 제일앞으로)

--- src/js/component/Shape/Line/index.js
+++ src/js/component/Shape/Line/index.js
@@ -2,7 +2,7 @@ import Shape from '../index';
 import GraphicElement from "../../GraphicElement";
 import {BOARD_ID, COLOR, GROUP, MENU_BAR} from "../../../service/constant";
 import ComponentRepository from "../../../service/ComponentRepository";
-import LineBorder from "../../Border/LineBorder";
+import LineBorder from '../../Border/LineBorder';
 import TransformManager from "../../../service/TransformManager";
 
 class Line extends Shape {
@@ -116,6 +116,8 @@ class Line extends Shape {
         if(ComponentRepository.getComponentById(itemMenubar.selectMenu).name !== 'mouse'){
             return;
         }
+
+        this.elem.parentNode?.appendChild?.(this.elem);
 
         board.destroyBorder();
         board.border = new LineBorder({
