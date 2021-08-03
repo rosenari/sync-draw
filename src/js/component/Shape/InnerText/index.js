28eed37193aa4760a80fdbb3897433e4e51d6f92
rosenari <rosenari88@gmail.com>
2021-08-03 10:19:41 +0900

    innerText 컴포넌트 생성

---
+++ src/js/component/Shape/InnerText/index.js
@@ -0,0 +1,108 @@
+import GraphicElement from '../../GraphicElement';
+import CustomElement from '../../CustomElement';
+import {getOverflowHeight, isOverflowHeight} from '../../../service/util';
+import ComponentRepository from '../../../service/ComponentRepository';
+import { BOARD_ID } from '../../../service/constant';
+
+export default class InnerText extends GraphicElement{
+    _foreignObj = null;
+    _textBox = null;
+
+    constructor({ parentId, id, x, y, width, height, classList, handlers }) {
+        super({ parentId, id, tagName: 'g', classList, handlers });
+
+        this.foreignObj = new GraphicElement({
+            parentId:this.id,
+            id:id + '-foreignobj',
+            tagName:'foreignObject'
+        });
+
+        this.textBox = new CustomElement({
+            parentId: this._foreignObj.id,
+            id: id + '-textbox',
+            tagName: 'div',
+            content: 'text',
+            handlers: {
+                dbClickHandler: (e) => {
+                    this._textBox.elem.setAttribute('contenteditable','true');
+                    this._textBox.elem.focus();
+
+                    destroyBorder();
+                },
+                clickHandler: (e) => {
+                    this.clickHandler(e);
+                },
+                blurHandler: (e) => {
+                    this._textBox.elem.setAttribute('contenteditable','false');
+                },
+                keyUpHandler: (e) => {
+                    if(isOverflowHeight(this._textBox.elem)){
+                        this.height += getOverflowHeight(this._textBox.elem);
+                    }
+
+                    destroyBorder();
+                }
+            }
+        });
+
+        this._textBox.elem.setAttribute('style','display:inline-block;width:100%;height:100%;word-break:break-word;user-select:none;');
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    get x() {
+        return +this.foreignObj.elem.getAttribute('x');
+    }
+
+    set x(value) {
+        this.foreignObj.elem.setAttribute('x', value);
+    }
+
+    get y() {
+        return +this.foreignObj.elem.getAttribute('y');
+    }
+
+    set y(value) {
+        this.foreignObj.elem.setAttribute('y', value);
+    }
+
+    get width() {
+        return +this.foreignObj.elem.getAttribute('width');
+    }
+
+    set width(value) {
+        this.foreignObj.elem.setAttribute('width', value);
+    }
+
+    get height() {
+        return +this.foreignObj.elem.getAttribute('height');
+    }
+
+    set height(value) {
+        this.foreignObj.elem.setAttribute('height', value);
+    }
+
+    get foreignObj() {
+        return this._foreignObj;
+    }
+
+    set foreignObj(value) {
+        this._foreignObj = value;
+    }
+
+    get textBox() {
+        return this._textBox;
+    }
+
+    set textBox(value) {
+        this._textBox = value;
+    }
+}
+
+function destroyBorder(){
+    const board = ComponentRepository.getComponentById(BOARD_ID);
+    board.destroyBorder();
+}
