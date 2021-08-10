e1563bbb19638454dd5dcb8267aa9bef050823cc
chris-92 <chris.92@kakaocorp.com>
2021-08-10 16:34:14 +0900

    새페이지, 저장 및 불러오기, 자동저장기능 개발

--- src/js/component/PageMenubar/index.js
+++ src/js/component/PageMenubar/index.js
@@ -1,6 +1,7 @@
 import CustomElement from '../CustomElement';
 import CustomButton from '../CustomButton';
 import {BUTTON, MENU_BAR} from '../../service/constant';
+import HistoryManager from '../../service/HistoryManager';
 import './index.css';
 
 export default class PageMenubar extends CustomElement{
@@ -24,7 +25,8 @@ export default class PageMenubar extends CustomElement{
             classList:['page-btn','page-new-btn'],
             handlers:{
                 clickHandler: function(e){
-                    console.log('new button !');
+                    const answer = confirm('정말 초기화 하시겠습니까 ?');
+                    if(answer) HistoryManager.clear();
                 }
             }});
 
@@ -36,7 +38,15 @@ export default class PageMenubar extends CustomElement{
             classList:['page-btn','page-save-btn'],
             handlers:{
                 clickHandler: function(e){
-                    console.log('save button !');
+                    const name = prompt('어떤이름으로 저장하시겠습니까 ?');
+                    if(!name) return;
+
+                    if(HistoryManager.isStoreName(name)){
+                        const answer = confirm('이미 존재하는 이름입니다. 덮어쓰시겠습니까 ?');
+                        if(!answer) return;
+                    }
+
+                    HistoryManager.storeCurrentPage(name);
                 }
             }});
 
@@ -48,7 +58,10 @@ export default class PageMenubar extends CustomElement{
             classList:['page-btn','page-load-btn'],
             handlers:{
                 clickHandler: function(e){
-                    console.log('load button !');
+                    const name = prompt('복구할 저장내역 이름을 입력하세요.');
+                    if(!name) return;
+
+                    HistoryManager.restorePage(name);
                 }
             }});
     }
