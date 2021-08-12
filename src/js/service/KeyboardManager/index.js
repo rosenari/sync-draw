bc27dd8b0cb6da33f3aca5e8de1fcfcd1dd28bf4
rosenari <rosenari88@gmail.com>
2021-08-12 13:21:42 +0900

    방향키 로직 변경

--- src/js/service/KeyboardManager/index.js
+++ src/js/service/KeyboardManager/index.js
@@ -29,20 +29,20 @@ class KeyboardManager {
             }else if(e.key === KEYEVENT.deleteKey){
                 board.border?.deleteHandler?.();
             }else if(e.key === KEYEVENT.LEFT){
-                board.border?.startPointInit();
-                board.border?.moveHandler({ dx: -10, dy: 0 });
+                board.border?.startPointInit?.();
+                board.border?.moveHandler?.({ dx: -10, dy: 0 });
                 board.border?.moveCompleteHandler();
             }else if(e.key === KEYEVENT.UP){
-                board.border?.startPointInit();
-                board.border?.moveHandler({ dx: 0, dy: -10 });
+                board.border?.startPointInit?.();
+                board.border?.moveHandler?.({ dx: 0, dy: -10 });
                 board.border?.moveCompleteHandler();
             }else if(e.key === KEYEVENT.RIGHT){
-                board.border?.startPointInit();
-                board.border?.moveHandler({ dx: 10, dy: 0 });
+                board.border?.startPointInit?.();
+                board.border?.moveHandler?.({ dx: 10, dy: 0 });
                 board.border?.moveCompleteHandler();
             }else if(e.key === KEYEVENT.DOWN){
-                board.border?.startPointInit();
-                board.border?.moveHandler({ dx: 0, dy: 10 });
+                board.border?.startPointInit?.();
+                board.border?.moveHandler?.({ dx: 0, dy: 10 });
                 board.border?.moveCompleteHandler();
             }
         });
