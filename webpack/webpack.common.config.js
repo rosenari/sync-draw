4ce08a2bb6293505528aa8733e8fcb6f58d92842
chris-92 <chris.92@kakaocorp.com>
2021-08-27 15:29:24 +0900

    ts환경구성

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -3,19 +3,26 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
-        syncdraw: './src/js/index.js'
+        syncdraw: './src/index.ts'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader'
+                loader: 'babel-loader' //weakmap을 ts-loader에서 지원하지않기 때문에 추가해야함.
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
             },
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
+            }
         ],
+    },
+    resolve: {
+        extensions: ['.ts', '...'], //기존해석확장자 + ts
     },
     plugins: [
         new HtmlWebpackPlugin({
